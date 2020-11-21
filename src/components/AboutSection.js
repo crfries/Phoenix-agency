e4e84efee97a9a2ebc6e2c3e862f9e2898884ca0
crfries <71576386+crfries@users.noreply.github.com>
2020-11-21 16:40:13 -0700

    services styles

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,10 +3,11 @@ import home1 from "../img/home1.png";
 
 //style
 import styled from "styled-components";
+import { About, Description, Hide, Image } from "../styles";
 
 const AboutSection = () => {
   return (
-    <AboutStyle>
+    <About>
       <Description>
         <div className="title">
           <Hide>
@@ -30,42 +31,8 @@ const AboutSection = () => {
       <Image>
         <img src={home1} alt="guy with camera"></img>
       </Image>
-    </AboutStyle>
+    </About>
   );
 };
-
-//styled component
-const AboutStyle = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-
-  h2 {
-    font-weight: lighter;
-  }
-`;
-
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default AboutSection;
