36b3e93a14c2218d8b3f295360fdc61b52ec97aa
crfries <71576386+crfries@users.noreply.github.com>
2020-11-21 17:09:33 -0700

    nav update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import GlobalStyle from "./components/GlobalStyle";
 
 //import pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <AboutUs />
       <GlobalStyle />
     </div>
